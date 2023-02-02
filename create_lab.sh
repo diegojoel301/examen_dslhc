f30aac90331843cfc9681488178173ee81a54314
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-02-02 19:20:24 -0400

    Update create_lab.sh

--- create_lab.sh
+++ create_lab.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop examen_lab_dslhc
-docker rm examen_lab_dslhc
+docker stop examen_lab_dslhc && docker rm examen_lab_dslhc
 
-docker build -t examen_lab_dslhc
+docker build -t examen_lab_dslhc .
 
-docker run -it examen_lab_dslhc --name examen_lab_dslhc
+docker run -d --name examen_lab_dslhc -it examen_lab_dslhc
 
-docker exec -it examen_lab_dslhc "service mysql restart"
+docker exec -it examen_lab_dslhc service mysql restart
+docker exec -it examen_lab_dslhc chmod +x script.sh
 docker exec -it examen_lab_dslhc "./script.sh"
